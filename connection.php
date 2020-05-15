db54360e37cc1745dcbad21722b01e016454fe96
tomcu07 <61151607+tomcu07@users.noreply.github.com>
2020-05-16 00:50:06 +0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+	$ambyar = mysqli_connect("localhost","root","","desakita");
+
+	/*if (mysqli_connect_errno())
+		{
+			echo "Database Gagal Terkoneksi";
+		}else{
+			echo "Database Berhasil Terkoneksi";
+		}*/
+?>
